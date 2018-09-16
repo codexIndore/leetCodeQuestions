a211b936a583fa0ee4cd7b07e2f0a58b1b7d9028
Nishant Jain <nishant@nishants-mbp.lan>
2018-09-15 19:52:24 -0700

    Added the link to the LeetCode and the question statement

--- src/main/scala/easy/ToLowerCase.scala
+++ src/main/scala/easy/ToLowerCase.scala
@@ -2,6 +2,25 @@ package easy
 
 import scala.collection.mutable.ListBuffer
 
+/**
+  * LeetCode: https://leetcode.com/problems/to-lower-case/description/
+  * Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+  *
+  *
+  *
+  * Example 1:
+  *
+  * Input: "Hello"
+  * Output: "hello"
+  * Example 2:
+  *
+  * Input: "here"
+  * Output: "here"
+  * Example 3:
+  *
+  * Input: "LOVELY"
+  * Output: "lovely"
+  */
 object ToLowerCase {
   def main(args: Array[String]): Unit = {
     println(toLowerCase("ABCD1234 $efg"))
